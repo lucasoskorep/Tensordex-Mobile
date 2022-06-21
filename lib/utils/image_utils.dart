dc4bf39c14b297e90f0e52433fd6cfe4d57a38ee
Lucas Oskorep <lucas.oskorep@gmail.com>
2022-06-21 19:14:58 -0400

    fixed image utils to use proper logging.

--- lib/utils/image_utils.dart
+++ lib/utils/image_utils.dart
@@ -2,10 +2,9 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:image/image.dart' as image_lib;
-import 'package:logger/logger.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:tensordex_mobile/utils/logger.dart';
 
-import 'logger.dart';
 
 /// ImageUtils
 class ImageUtils {
